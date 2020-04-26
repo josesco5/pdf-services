537d04289908c922adf798d467fc30f24bc380ee
José Escorche <josesco5@gmail.com>
2020-04-26 11:42:00 -0400

    Initial commit

---
+++ src/main/java/com/avilapps/pdf_services/PdfServicesApplication.java
@@ -0,0 +1,13 @@
+package com.avilapps.pdf_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avilapps/pdf_services/PdfServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avilapps.pdf_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
