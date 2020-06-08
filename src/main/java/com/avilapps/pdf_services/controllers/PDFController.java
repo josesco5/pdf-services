25fe703e4dce4eb5214030788d0d55862a3f26e8
José Escorche <josesco5@gmail.com>
2020-06-07 21:35:02 -0400

    Make foliation on PDF controller

--- src/main/java/com/avilapps/pdf_services/controllers/PDFController.java
+++ src/main/java/com/avilapps/pdf_services/controllers/PDFController.java
@@ -3,12 +3,14 @@ package com.avilapps.pdf_services.controllers;
 import com.avilapps.pdf_services.dto.FoliationRequest;
 import com.avilapps.pdf_services.dto.FoliationResponse;
 import com.avilapps.pdf_services.dto.PageCountResponse;
+import com.avilapps.pdf_services.services.Foliator;
 import com.avilapps.pdf_services.services.PageCounter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
 import java.io.IOException;
 
 @RestController
@@ -19,6 +21,9 @@ public class PDFController {
     @Autowired
     private PageCounter pageCounter;
 
+    @Autowired
+    private Foliator foliator;
+
     @GetMapping("/pages-count")
     public PageCountResponse countPDFPages(@RequestParam(value="url") String fileUrl) throws IOException {
         LOG.info("Counting pages for " + fileUrl);
@@ -27,9 +32,11 @@ public class PDFController {
     }
 
     @PostMapping("/foliate")
-    public FoliationResponse foliate(@RequestBody FoliationRequest request) {
+    public FoliationResponse foliate(@RequestBody FoliationRequest request) throws IOException {
         LOG.info("Foliating PDF file at " + request.getFoliationSettings().getFileUrl());
         String fileUrl = request.getFoliationSettings().getFileUrl();
+        File result = foliator.foliate(request.getFoliationSettings());
+        result.deleteOnExit();
         return new FoliationResponse(fileUrl, fileUrl);
     }
 }
